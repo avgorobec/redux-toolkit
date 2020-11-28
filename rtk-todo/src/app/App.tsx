bdb6f0683268068339ef7e710f95c9fa7b09ada6
Алексей Горобец <a.gorobec@forvard.me>
2020-11-28 23:46:19 +0300

    Done

--- rtk-todo/src/app/App.tsx
+++ rtk-todo/src/app/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+
+import AddTodo from 'features/todoList/AddTodo';
+import TodoList from 'features/todoList/TodoList';
+import Footer from 'features/visibilityFilter/Footer';
 import './App.css';
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <AddTodo />
+      <TodoList />
+      <Footer />
+    </div>
+  );
 }
